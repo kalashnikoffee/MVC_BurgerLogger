cbb21c71cf2995694f7ccb40634edf4b2491c679
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-20 21:04:49 -0800

    orm.js assembly

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,24 @@
 var connection = require("./connection");
+
 var orm = {
-    all: function(){
-
-    };
-    create: function(){
-
-    };
-    update: function(){
-
-    };
-}
+    all: function(tableInput, cb){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    create: function(tableInput, val, cb){
+        connection.query("INSERT INTRO " + tableInput + " (burger_name) VALUES ('" + val + "');",
+        function(err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    update: function(tableInput, condition, cb) {
+        connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err, res) {
+            if (err) throw err;
+            cb(res);
+        })
+    }
+};
