2f8354bb48f7286ffe7011ee10bd4624d76cbd94
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-22 23:10:10 -0800

    connection.js edit

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 else {
     connection = mysql.createConnection({
         host: "localhost",
-        // PORT: 3306,
+        PORT: 3306,
         user: "root",
         password: "shit",
         database: "burgers_db"
