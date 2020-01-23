68017dc84ae1b011861334a6a06daff077634b4c
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-22 23:09:13 -0800

    connection.js edit

--- config/connection.js
+++ config/connection.js
@@ -11,6 +11,11 @@ else {
         user: "root",
         password: "shit",
         database: "burgers_db"
+        // host: "rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        // // PORT: 3306,
+        // user: "v2hsnx5xkwr9xwxx",
+        // password: "st7b2qirs634mnkq",
+        // database: "j3rmxe7cj1aoxdfv"
     })
 };
 
